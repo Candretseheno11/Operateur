1c513b18f546387cf9f7b0792baba527c1c94296
christon <andretsehenoc@gmail.com>
2026-07-20 09:44:43 +0300

    maj

--- app/Models/ClientModel.php
+++ app/Models/ClientModel.php
@@ -14,10 +14,28 @@ class ClientModel extends Model
     {
         return $this->find($id);
     }
-    public function getClientByTelephone($telephone)
+    public function getClientByTelephones($telephone)
     {
         return $this->where('telephone', $telephone)->first();
 
     }
+    public function getClientByTelephone(string $telephone)
+    {
+        return $this->select('clients.*, comptes.id as id_compte, comptes.solde, prefixes.prefixe')
+            ->join('comptes', 'comptes.id_client = clients.id', 'left')
+            ->join('prefixes', 'prefixes.id = clients.id_prefixe', 'left')
+            ->where('clients.telephone', $telephone)
+            ->first();
+    }
+    public function getPrefixeByNumber(string $telephone)
+    {
+        $codePrefixe = substr($telephone, 0, 3); // Extrait les 3 premiers chiffres (ex: 033 ou 037)
 
+        $db = \Config\Database::connect();
+        return $db->table('prefixes')
+            ->where('prefixe', $codePrefixe)
+            ->where('actif', 1)
+            ->get()
+            ->getRowArray();
+    }
 }
